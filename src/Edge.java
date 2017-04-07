81fd7e6feba8e0c3e20b3b3f04e888f902cdee17
Matthew Bowers <matthew.allen.bowers@gmail.com>
2017-04-07 08:18:08 -0400

    inital commit

---
+++ src/Edge.java
@@ -0,0 +1,17 @@
+/**
+ * Edge.java 
+ * A java class to represent and edge between vertices in the graph
+ * Author: Matthew Bowers
+ * Last Update: 
+ */
+
+public class Edge{
+    /**
+     * origin of a directed graph
+     */
+    Vertex u;
+    /**
+     * end of a directed graph
+     */
+    Vertex v;
+}
---
+++ src/Graph.java
@@ -0,0 +1,8 @@
+/**
+ * Java class to represent a graph of edges and nodes
+ */
+public class Graph{
+    // variables
+    Node[] nodes;
+    Edge[] edges;
+}
