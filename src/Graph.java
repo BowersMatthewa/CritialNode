005b2de2db4a146c1d6f410ed8db021c974c4fa7
Matthew Bowers <Matthew.Allen.Bowers@gmail.com>
2017-04-13 15:06:32 -0400

    added some getters and setters to Node and added the ability to add a Node to the Graph

--- src/Graph.java
+++ src/Graph.java
@@ -8,6 +8,7 @@ public class Graph{
     // variables
     AdjacencyList list;
 	AdjacencyMatric matrix;
+	ArrayList<Node> nodes;
 	int nodeCount, edgeCount;
 
 	public Graph(int nodeCount, int edgeCount) throws IllegalArgumentExeption{
@@ -26,14 +27,38 @@ public class Graph{
 		if(u > nodeCount || v > nodeCount || u<0 || v<0){
 			throw new IllegalArgumentException("attempting to add edge to one or more edges which do not exist");
 		}
+		if(!u.isValid || v.isValid()){
+			throw new IllegalArgumentException("attempting to add edge to a node which has been removed");
+		}
 		list.addEdge(u,v);
 		matrix.addEdge(u,v);
 	}
 
 	public void removeNode(int node)throws IllegalArgumentException{
-		if(node < 0 || node > nodeCount || !node.getValid()){
+		if(node < 0 || node > nodeCount || !node.isValid()){
 			throw new IllegalArgumentException("Attempting to remove an invalid node");
 		}
 	}
 
+	/**
+	 * addNode - adds a node to the graph 
+	 *
+	 * @param newNode - the node which is too be added
+	 * @throws IllegalArgumentException - if attempting to add a node which is already 
+	 * 				in the graph
+	 */
+	public void addNode(Node newNode){
+		if(nodes.contains(newNode) && newNode.isValid()){
+			throw new IllegalArgumentException("Node is already in the graph")
+		}else if(nodes.contains(newNode) && !newNode.isValid()){
+			newNode.setValid(1);
+		}else{
+			nodes.add(newNode);
+			// add this new nodes edges to the adjacency list
+			for(Node n : newNode.getNeighbors()){
+				// TODO: Finish this
+			}
+		}
+		
+	}
 }
--- src/Node.java
+++ src/Node.java
@@ -9,29 +9,51 @@ package com.git.bowersmatthewa.criticalnode
  */
 
 public class Node {
-	ArrayList<Node> =  edges;
+	ArrayList<Node> =  neighbors;
 	int name;
+	boolean valid;
 
 	public Node Node(int name, ArrayList<Node> edges) throws IllegalArgumentException {
 		if(name.equals(""){
 			throw new IllegalArgumentException("Node name may not be empty");
 		}
 		else{
-			this.edges = edges;
+			this.neighbors = edges;
 			this.name = name;
+			this.valid = 1;
 		}
 	}
 
+	/**
+	 * addNeighbor - adds a node to the list of neighbors
+	 *
+	 * @param newNeighbor - the node to add as a neighbor
+	 * @throws IllegalArgumenException - if attempting to add a neighbor which already exists
+	 */
 	public void addNeighbor(Node newNeighbor) throws IllegalArgumentException { 
+			if(neighbors.contains(newNeighbor)){
+				throw new IllegalArgumentException("Edge already exists");
+			}
 			edges.add(newNeighbor);
 	}
+
+	/**
+	 * getNeighbors - returns the list of neighbors
+	 */
+	public ArrayList<Node> getNeighbors(){
+		return neighbors;
+	}
+
+
 	public String getName(){
 		return name;
 	}
 
+
 	public ArrayList<Edge> getEdges(){
 		return edges;
 	}
+
 
 	public String toString(){
 		StringBuilder sb = new StringBuilder();
@@ -42,6 +64,22 @@ public class Node {
 		}
 
 		return (sb.toString());
+	}
+
+	/**
+	 * isValid - checks if the node is currently a part of the graph
+	 */
+	public boolean isValid(){
+		return valid;
+	}
+
+	/**
+	 * setValid - sets the valid bit of the node
+	 *
+	 * @param newVal boolean representing if the node is valid or not
+	 */
+	public void setValid(boolean newVal){
+		valid = newVal;
 	}
 }
 
